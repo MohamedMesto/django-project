809cc56398eed47c79fba1e58ae4493f9438185c
MohamedMesto <mohamedmesto111@gmail.com>
2024-09-09 18:37:08 +0200

    Get , Post

--- hello_world/views.py
+++ hello_world/views.py
@@ -4,5 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(index):
+""" def index(index):
     return HttpResponse("Salam, world!")
+"""
+""" 
+def index(request):
+
+   if request.method == "GET":
+       return HttpResponse("This was a GET request")
+   elif request.method == "POST":
+       return HttpResponse("This was a POST request") 
+"""
+
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
